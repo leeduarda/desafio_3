d9f4237f3509e1b326ac04e6055431567da00abf
Luis Santos <leticia.eduarda.santos2016@gmail.com>
2024-06-13 20:43:40 -0300

    feat:secao 2 da homePage

--- src/pages/Home/homePage.tsx
+++ src/pages/Home/homePage.tsx
@@ -1,6 +1,12 @@
 const homePage = () => {
   const imageHome1 =
     "https://imgsdesafio3.s3.us-east-2.amazonaws.com/homeImgs/imageHome1.png";
+  const imgDiningHome =
+    "https://imgsdesafio3.s3.us-east-2.amazonaws.com/homeImgs/diningHomeS2.png";
+  const imgLivingHome =
+    "https://imgsdesafio3.s3.us-east-2.amazonaws.com/homeImgs/livingHomeS2.png";
+  const imgBedrooHome =
+    "https://imgsdesafio3.s3.us-east-2.amazonaws.com/homeImgs/bedrooHomeS2.png";
   return (
     <>
       <section className="relative">
@@ -24,6 +30,41 @@ const homePage = () => {
           <button className="bg-customPrimary py-4 px-11 font-poppins text-base font-bold text-customWhite transition-all hover:text-customPrimary hover:bg-customYellow4 hover:ring-1 hover:ring-customPrimary">
             BUY NOW
           </button>
+        </div>
+      </section>
+
+      <section className="mt-6">
+        <h2 className="font-poppins font-bold text-2xl text-customBlack2 text-center">
+          Browse The Range
+        </h2>
+        <p className="font-poppins text-base text-customGray6 text-center mb-10">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+        <div className="flex justify-center sm:flex sm:flex-row sm:justify-center flex-wrap text-center">
+          <div className="p-5 w-96">
+            <img
+              className="mb-5 w-96"
+              src={imgDiningHome}
+              alt="image dinner space"
+            />
+            <p className="font-poppins font-semibold text-lg">Dining</p>
+          </div>
+          <div className="p-5 w-96">
+            <img
+              className="mb-5 w-96"
+              src={imgLivingHome}
+              alt="image living room"
+            />
+            <p className="font-poppins font-semibold text-lg">Living</p>
+          </div>
+          <div className="p-5 w-96">
+            <img
+              className="mb-5 w-96"
+              src={imgBedrooHome}
+              alt="image bedroom"
+            />
+            <p className="font-poppins font-semibold text-lg">Bedroom</p>
+          </div>
         </div>
       </section>
     </>
